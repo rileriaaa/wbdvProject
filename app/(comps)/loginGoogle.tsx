0b412cf81dee3ac1cb9a2ff00be8a61b165417a1
shlynav.tiff <ajnaval67@gmail.com>
2026-04-23 21:31:30 +0800

    supabase

---
+++ app/(comps)/loginGoogle.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { createClient } from '../lib/supabase/client'
+
+export default function LoginButton() {
+    const handleLogin = async () => {
+        const supabase = createClient()
+        await supabase.auth.signInWithOAuth({
+            provider: 'google',
+            options: {
+                redirectTo: `${location.origin}/auth/callback`,
+            },
+        })
+    }
+
+    return <button onClick={handleLogin}>Sign in with Google</button>
+}
